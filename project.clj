2f9fed0b9da2274e6caf46cc4c812d630584925a
iain <iain@soulflyer.co.uk>
2016-01-10 14:57:12 +0700

    Added slf4j to suppress excess output

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.3"]
-                 [image-lib "0.1.0-SNAPSHOT"]]
+                 [image-lib "0.1.0-SNAPSHOT"]
+                 [org.slf4j/slf4j-nop "1.7.12"]]
   :main ^:skip-aot find-projects.core
 ;;  :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
